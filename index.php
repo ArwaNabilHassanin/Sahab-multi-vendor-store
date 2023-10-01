49f106b0ed4a2c3c08293e3f7bb5bd9eaf6b3ecf
ArwaNabilHassanin <arwanabil002@gmail.com>
2023-10-01 11:00:55 +0200

    create laravel project

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+ class task {
+
+     public function sum($num1,  $num2) {
+     $nummber3= $num1+$num2;
+    echo "the result is : $nummber3";
+    echo "hello , world";
+   }}
+
+   $addition =new task();
+   $addition->sum(5,5);
+  
+  // $color= "red";
+  // $arr=array(
+  //   "red"=> "#ff000",
+  //   "green"=>"#00ff00",
+  //   "blue"=>"#00ff"
+  // );
+  // $num=true;
+  // var_dump($arr) . "<br>";
+  // // echo "the color of sky is " .$color."<br>";
+  // // echo gettype($color) ."<br>";
+  // // define("SITE_URL", "https://www.google.com/");
+  // // echo 'Thank you for visiting - ' . SITE_URL;
+  // // echo"<h1> page</h1>"
+  // define("array",["1","2","3"]);
+  // echo array[0];
+  // define("cars", ["Alfa Romeo","BMW","Toyota"]);
+  // echo cars[0];
+  // // echo" $arr[0]"
+?>
